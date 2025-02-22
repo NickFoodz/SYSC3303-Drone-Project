f1358366714a2c0a8b854e57828d4213577c47a9
ThomasImbert7 <165952996+ThomasImbert7@users.noreply.github.com>
2025-02-22 17:42:00 -0500

    file path for csv file

--- src/FireIncidentSubsystem.java
+++ src/FireIncidentSubsystem.java
@@ -13,7 +13,7 @@ import java.util.List;
 public class FireIncidentSubsystem implements Runnable {
     private final Scheduler scheduler; //Scheduler
     private final Simulation simulation; //Simulation **NEEDED**
-    private final String eventFilePath = "Sample_event_file.csv"; //File path to the .csv
+    private final String eventFilePath = "SYSC3303-Drone-Project-master/Sample_event_file.csv"; //File path to the .csv
     boolean EOF; //end of file reached
     List<FireEvent> allEvents;
 

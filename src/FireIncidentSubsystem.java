625e2dc1991a69d287231c0cb945d7c0c8991ad2
Thomas Imbert <thomasimbert@cmail.carleton.ca>
2025-03-16 02:40:44 -0400

    Added simulation functionality back

--- src/FireIncidentSubsystem.java
+++ src/FireIncidentSubsystem.java
@@ -7,14 +7,16 @@
 import java.io.*;
 import java.net.*;
 import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
 
 public class FireIncidentSubsystem {
-    private final String eventFilePath = "Sample_event_file.csv"; //File path to the .csv
+    private final String eventFilePath = "SYSC3303-Drone-Project-master/Sample_event_file.csv"; //File path to the .csv
     boolean EOF; //end of file reached
     List<FireEvent> allEvents;
+    private static Simulation simulation;
     private DatagramSocket FISSocket;
 
 
@@ -25,6 +27,7 @@ public class FireIncidentSubsystem {
     public FireIncidentSubsystem() {
         EOF = false;
         allEvents = new ArrayList<>();
+        simulation = new Simulation(1, this);
         try {
             FISSocket = new DatagramSocket(5999);
         } catch (SocketException e) {}
@@ -49,8 +52,6 @@ public class FireIncidentSubsystem {
                     String type = info[2];
                     String severity = info[3];
                     FireEvent event = new FireEvent(time, zoneID, type, severity);
-                    String eventStr = event.summarizeEvent();
-                    sendToScheduler(eventStr);
 
                     //Checks the format of the event for the drone in UDP
                     //System.out.println(event.summarizeEvent());
@@ -62,7 +63,7 @@ public class FireIncidentSubsystem {
 
             //sorts all events by timestamp so we can send them out to the simulation in order.
             allEvents.sort(Comparator.comparing(FireEvent::getTime));
-
+            System.out.println(allEvents);
 
         } catch (IOException ex) {
             throw new RuntimeException(ex);
@@ -82,15 +83,26 @@ public class FireIncidentSubsystem {
             DatagramPacket outgoing = new DatagramPacket(outgoingEvent, eventInfo.length(), InetAddress.getLocalHost(), 6001);
             FISSocket.send(outgoing);
             System.out.println("Fire Incident Subsystem Sent: " + eventInfo +"\n");
-            try {
-                Thread.sleep(5000);
-            } catch (InterruptedException e) {
-                throw new RuntimeException(e);
-            }
         } catch (IOException e) {
             System.out.println("Could not send event");
         }
 
+    }
+
+    public synchronized void consumeEvent(LocalTime currentSimTime) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm:ss");
+        while (!allEvents.isEmpty() && allEvents.get(0).getTime().equals(currentSimTime.format(formatter))) {
+            System.out.println(allEvents);
+            FireEvent event = allEvents.remove(0);
+            //SENDING EVENT TO SCHEDULER:
+            String eventStr = event.summarizeEvent();
+            sendToScheduler(eventStr);
+        }
+
+        if(allEvents.isEmpty()){
+            notifyAll();
+            simulation.endSimulation();
+        }
     }
 
 
@@ -102,6 +114,8 @@ public class FireIncidentSubsystem {
         FireIncidentSubsystem fis = new FireIncidentSubsystem();
         System.out.println("Fire Incident Subsystem Online\n");
         fis.getData();
+        Thread simulationThread = new Thread(simulation);
+        simulationThread.start();
         while(true){
         //exist (wait for shutdown signal)
         }

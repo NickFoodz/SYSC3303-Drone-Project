90fb9fc8cb4e78ef6b5ec27fe2d757426a2721c6
NickF <npfuda@gmail.com>
2025-03-30 17:12:12 -0400

    Fixed drone fault handling test

--- src/DroneSubsystemTest.java
+++ src/DroneSubsystemTest.java
@@ -9,7 +9,6 @@ class DroneSubsystemTest {
     @Test
     void testInitializeDrones() {
         droneSubsystem.initializeDrones();
-
         assertEquals(3, droneSubsystem.getDroneList().size(), "all drones are initialized");
         droneSubsystem.TESTING_closeSockets();
     }
@@ -18,10 +17,13 @@ class DroneSubsystemTest {
     void testDroneStateChange() throws InterruptedException {
         droneSubsystem.initializeDrones();
         DroneSubsystem.Drone drone = droneSubsystem.getDroneList().get(0);
+        DroneSubsystem.Drone drone2 = droneSubsystem.getDroneList().get(1);
+        DroneSubsystem.Drone drone3 = droneSubsystem.getDroneList().get(2);
 
         assertEquals(DroneSubsystem.Drone.droneState.IDLE, drone.getState(), "drone starts in state IDLE");
 
-        FireEvent fireEvent = new FireEvent("13:00:05",3,"FIRE_DETECTED","Low");
+        //Regular event
+        FireEvent fireEvent = new FireEvent("13:00:05",3,"FIRE_DETECTED","Low", "null");
 
         drone.startEvent(fireEvent);
 
@@ -30,6 +32,31 @@ class DroneSubsystemTest {
         assertEquals("DEPLOYINGAGENT", drone.getLog().get(2), "drone moves onto DEPLOYINGAGENT state");
         assertEquals("RETURNING", drone.getLog().get(3), "drone moves onto RETURNING state");
         assertEquals("IDLE", drone.getLog().get(4), "drone returns to IDLE state");
+
+        //Stuck event
+        FireEvent stuckEvent = new FireEvent("14:00:00",3,"FIRE_DETECTED","Low", "Drone Stuck");
+        drone2.startEvent(stuckEvent);
+        assertEquals("IDLE", drone2.getLog().get(0), "drone starts at IDLE state");
+        assertEquals("ENROUTE", drone2.getLog().get(1), "drone moves onto ENROUTE state");
+        assertEquals("RETURNING", drone2.getLog().get(2), "drone moves onto RETURNING state");
+        assertEquals("IDLE", drone2.getLog().get(3), "drone returns to IDLE state");
+
+        FireEvent nozzleEvent = new FireEvent("14:00:00",3,"FIRE_DETECTED","Low", "Nozzle Jammed");
+        drone3.startEvent(nozzleEvent);
+        assertEquals("IDLE", drone3.getLog().get(0), "drone starts at IDLE state");
+        assertEquals("ENROUTE", drone3.getLog().get(1), "drone moves onto ENROUTE state");
+        assertEquals("DEPLOYINGAGENT", drone3.getLog().get(2), "drone moves onto DEPLOYINGAGENT state");
+        assertEquals("RETURNING", drone3.getLog().get(3), "drone moves onto RETURNING state");
+        assertEquals("IDLE", drone3.getLog().get(4), "drone returns to IDLE state");
+        assertEquals("DISABLED", drone3.getLog().get(5), "drone moves onto RETURNING state");
+
+
+
+
         droneSubsystem.TESTING_closeSockets();
+
+
+
+
     }
 }
